1a4ad366f6f6abe49d5bbf6cb623ee3b31bce25f
You <duug@naver.com>
2022-07-05 23:46:12 +0900

    lombok의 기능, 사용법

---
+++ src/test/java/com/example/account/AccountDtoTest.java
@@ -0,0 +1,23 @@
+package com.example.account;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDtoTest {
+    @Test
+    void accountDto() {
+        //given
+        //when
+        //then
+
+        AccountDto accountDto = new AccountDto("accountNumber",
+                "summer",
+                LocalDateTime.now());
+
+        System.out.println(accountDto.getAccountNumber());
+        System.out.println(accountDto.toString());
+    }
+}
