f4ca658668cef21104e1e8e2cf471f29e6122aee
You <duug@naver.com>
2022-07-06 12:20:31 +0900

    계좌 생성 API

--- src/main/java/com/example/account/domain/AccountUser.java
+++ src/main/java/com/example/account/domain/AccountUser.java
@@ -1,12 +1,14 @@
 package com.example.account.domain;
 
-import com.example.account.type.AccountStatus;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Getter
@@ -14,23 +16,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity // 설정 파일
+@Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Account {
-    @Id // Primary Key로 지정
+public class AccountUser {
+    @Id
     @GeneratedValue
     private Long id;
 
-    @ManyToOne
-    private AccountUser accountUser;
-    private String accountNumber;
-
-    @Enumerated(EnumType.STRING) // enum에 있는 데이터의 index가 아니라 데이터 자체로 저장됨
-    private AccountStatus accountStatus;
-    private Long balance;
-
-    private LocalDateTime registerAt;
-    private LocalDateTime unRegisteredAt;
+    private String name;
 
     @CreatedDate
     private LocalDateTime createdAt;
